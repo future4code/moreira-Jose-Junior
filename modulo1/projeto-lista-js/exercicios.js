bab0c21634dc8ff2012697997443bcb4d670c1b1
unknown <antonio.santos2@ufpe.br>
2021-12-06 06:41:27 -0300

    Atualizando lista

--- modulo1/projeto-lista-js/exercicios.js
+++ modulo1/projeto-lista-js/exercicios.js
@@ -17,53 +17,73 @@ function imprimeMensagem() {
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
-function calculaAreaRetangulo(altura, largura) {
+function calculaAreaRetangulo() {
   // implemente sua lógica aqui
+  const altura = prompt("Digite a altura!")
+  const largura = prompt("Digite a largura!")
  const reto = altura * largura
-return reto
+console.log(reto)
 }
 
 // EXERCÍCIO 02
 function imprimeIdade() {
   // implemente sua lógica aqui
-
+const anoAtual = prompt("Digite o ano que estamos!")
+const anoNasci = prompt("Em qual ano você nasceu?")
+const anos = anoAtual - anoNasci
+console.log(anos)
 }
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
   // implemente sua lógica aqui
-
+return IMC = peso/(altura*altura)
 }
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
-
+const nome = prompt("Digite seu nome!")
+const idade = prompt("Quantos anos você tem?")
+const email = prompt("Digite seu e-mail.")
+const resultado = `Meu nome é ${nome}, tenho ${idade} anos, e o meu e-mail é ${email}.`
+console.log(resultado)
 }
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
-
+  const cor1 = prompt("Digite a primeria de três cores prediletas!")
+  const cor2 = prompt("Digite a segunda de três cores prediletas!")
+  const cor3 = prompt("Digite a terceira de três cores prediletas!")
+  const array1 = [cor1,cor2,cor3]
+  console.log(array1)
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
+  const frasemai = string.toUpperCase()
+    return frasemai
 
 }
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
   // implemente sua lógica aqui
+  const esp = custo/valorIngresso
+  return esp
 
 }
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
-
+const fraseum = Number(("Digite uma frase!"))
+const frasedois = (Number("Digite outra frase!"))
+const tamanho = fraseum == frasedois
+return tamanho
 }
 
 // EXERCÍCIO 09
