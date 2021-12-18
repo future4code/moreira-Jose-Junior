82fa8e7c52102e033e024784a34329ef1e8887b7
unknown <antonio.santos2@ufpe.br>
2021-12-17 21:41:34 -0300

    Criando meu portfolio!

---
+++ modulo1/css-bugfixes/Exercício de hoje/index.js
@@ -0,0 +1,5 @@
+console.log (ordemAlfa)=prompt("Digite um nome próprio")
+function ordena(){
+let ordemAlfa = new Array('Gabriel', 'Alex', 'Victor', 'Mabel', 'Roger');
+console.log(ordemAlfa.sort().join(' '));
+}
