1721d8ed5bb879738bcf22ad24764e27138b4e56
antoniosantos2 <antonio.santos2@ufpe.br>
2022-02-24 17:12:11 -0300

    Rotas criadas

--- modulo3/projeto-cookenu/cookenu/src/App.js
+++ modulo3/projeto-cookenu/cookenu/src/App.js
@@ -1,18 +1,10 @@
 import React from "react"
-import AddRecipesPage from "./pages/AddRecipesPage/AddRecipesPage"
-import LoginPage from "./pages/LoginPage/LoginPage"
-import RecipeDetailPage from "./pages/RecipeDetailPage/RecipeDetailPage"
-import RecipesListPage from "./pages/RecipesListPage/RecipesListPage"
-import SignUpPage from "./pages/SignUpPage/SignUpPage"
+import Router from "./routes/Router"
 
 const App = () => {
   return (
     <div>
-     <AddRecipesPage/>
-     <LoginPage/>
-     <RecipeDetailPage/>
-     <RecipesListPage/>
-     <SignUpPage/>
+     <Router/>
     </div>
   );
 }
--- modulo3/projeto-cookenu/cookenu/src/routes/Router.js
+++ modulo3/projeto-cookenu/cookenu/src/routes/Router.js
@@ -1,2 +1,35 @@
 import React from "react"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import AddRecipesPage from "../pages/AddRecipesPage/AddRecipesPage"
+import LoginPage from "../pages/LoginPage/LoginPage"
+import RecipeDetailPage from "../pages/RecipeDetailPage/RecipeDetailPage"
+import RecipesListPage from "../pages/RecipesListPage/RecipesListPage"
+import SignUpPage from "../pages/SignUpPage/SignUpPage"
+import ErrorPage from "../pages/ErrorPage/ErrorPage"
+
+
+
+
+const Router = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path='/adicionar-receita' element={<AddRecipesPage/>}/>
+
+                <Route path='/login' element={<LoginPage/>}/>
+
+                <Route path='/detalhe/:id' element={<RecipeDetailPage/>}/>
+
+                <Route path='/' element={<RecipesListPage/>}/>
+
+                <Route path='/cadastro' element={<SignUpPage/>}/>
+
+                <Route path="*"  element={<ErrorPage/>}/>
+
+            </Routes>
+
+        </BrowserRouter>
+    )
+}
+
+export default Router
