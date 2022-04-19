024c3525babc194a3ddfc071ce27ba5275722647
antoniosantos2 <antonio.santos2@ufpe.br>
2022-04-19 09:47:02 -0300

    1º quesito

---
+++ modulo5/classes-encapsulamento/src/index.ts
@@ -0,0 +1,22 @@
+class UserAccount {
+    private cpf: string;
+    private name: string;
+    private age: number;
+    private balance: number = 0;
+    private transactions: Transaction[] = [];
+  
+    constructor(
+       cpf: string,
+       name: string,
+       age: number,
+    ) {
+       console.log("Chamando o construtor da classe UserAccount")
+       this.cpf = cpf;
+       this.name = name;
+       this.age = age;
+    }
+  
+  }
+
+  /* a) O constructor serve para executar determinadas funções 
+  que devem ser realizadas quando uma instãncia da classe é criada.*/
